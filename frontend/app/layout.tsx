fa421e674dc90d02b2abf9dd6b879719d38ce263
fungc-io <fung@oursky.com>
2026-03-25 20:10:50 +0000

    chore: remove intl-tel-input CDN script and delete PageScripts

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -201,11 +201,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <Script id="gtm-init" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KTHFL6S');`}
         </Script>
-        {/* Load before page scripts: Webflow inline scripts use intlTelInput via PageScripts */}
-        <Script
-          src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/intlTelInput.min.js"
-          strategy="beforeInteractive"
-        />
         </NextIntlClientProvider>
       </body>
     </html>
