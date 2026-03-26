96e1c3addd5b5924091a329c06f290d9a546b94b
fungc-io <fung@oursky.com>
2026-03-26 09:28:14 +0000

    feat: use PlausibleLink in ToolPopup instead of CSS class tracking

--- frontend/components/tools/ToolPopup.tsx
+++ frontend/components/tools/ToolPopup.tsx
@@ -1,19 +1,21 @@
+import PlausibleLink from '@/components/PlausibleLink';
+
 export default function ToolPopup() {
   return (
     <div className="tool-popup">
       <h1 className="dev-tool-popup-heading">This Dev Tool is crafted by Authgear</h1>
       <p className="paragraph-20">Open source Auth0/Clerk/Firebase alternative. Passkeys, SSO, MFA, passwordless, biometric login.</p>
       <div className="tool-popup-wrapper">
-        <a href="https://portal.authgear.com/" target="_blank" className="tool-popup-button plausible-event-name--tool-popup-signup-click w-inline-block">
+        <PlausibleLink href="https://portal.authgear.com/" target="_blank" className="tool-popup-button w-inline-block" eventName="tool-popup-signup-click">
           <div>Start building for Free</div>
-        </a>
-        <a href="https://github.com/authgear/authgear-server" target="_blank" className="tool-popup-button secondary plausible-event-name--tool-github-click w-inline-block">
+        </PlausibleLink>
+        <PlausibleLink href="https://github.com/authgear/authgear-server" target="_blank" className="tool-popup-button secondary w-inline-block" eventName="tool-github-click">
           <div>Star us on</div>
           <img src="https://img.shields.io/github/stars/authgear/authgear-server" width="110px" alt="" />
-        </a>
+        </PlausibleLink>
       </div>
       <div className="tool-popup-close-button-wrapper">
-        <a href="#" className="tool-popup-close-button plausible-event-name--popup-close-click w-button">Close</a>
+        <PlausibleLink href="#" className="tool-popup-close-button w-button" eventName="popup-close-click">Close</PlausibleLink>
       </div>
     </div>
   );
